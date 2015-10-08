8ddfd3bd85d34d772d763e1873f98b52f6ebddad
tarzak <jobtarzak@gmail.com>
2015-10-08 10:43:14 +0300

    test(tabify) add new tab

--- test/tabify.js
+++ test/tabify.js
@@ -4,6 +4,18 @@
     let Tabify = require('../lib/tabify.js'),
         test   = require('tape');
 
+    test('add: new tab', t => {
+        let tabify = Tabify,
+            fn     = function () {
+                tabify.add({name: 'testTab'});
+                
+                return tabify.get();
+            };
+
+        t.deepEqual(fn(), [{name: 'testTab'}]);
+        t.end();
+    });
+
     test('get: no argument', t => {
         let tabify = Tabify;
 
